bdf86f35878a64d5e55c3612ed69fecfee680eac
Oimah <obiora@dealdey.com>
2015-12-30 12:38:41 +0000

    correction on blog

--- exercises/js/load.js
+++ exercises/js/load.js
@@ -1,6 +1,6 @@
 
 function LoadAjaxBlog(me){
-  $(me).parent().parent().find("p").load( "blog/" + $(me).attr("data-file") , function( response, status, xhr ) {
+  $(me).parent().parent().find("p").load( "blog/" + $(me).data("file") , function( response, status, xhr ) {
   if ( status == "error" ) {
     var msg = "Sorry but there was an error: ";
     $( ".error" ).html( msg + xhr.status + " " + xhr.statusText );
