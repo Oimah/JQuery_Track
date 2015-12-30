38f8048921dcf432fa31acd87f7b286e4cf9f30f
Oimah <obiora@dealdey.com>
2015-12-30 11:16:01 +0000

    implemented oop on tab

--- exercises/js/load.js
+++ exercises/js/load.js
@@ -1,5 +1,5 @@
 
-function loadAjaxBlog(me){
+function LoadAjaxBlog(me){
   $(me).parent().parent().find("p").load( "blog/" + $(me).attr("data-file") , function( response, status, xhr ) {
   if ( status == "error" ) {
     var msg = "Sorry but there was an error: ";
--- exercises/js/tabs.js
+++ exercises/js/tabs.js
@@ -1,34 +1,54 @@
 var prevTabText;
 
-$("div.module h2").each(function(){
-  $(".dynamicUl").append("<li class="+ $(this).text().toLowerCase() +">" + $(this).text() + "</li>")
-})
-$("#blog p.excerpt ").hide();
+function CreateTab(){
+this.run();
+}
+
+CreateTab.prototype.tabButtons = function(){
+  $("div.module h2").each(function(){
+    $(".dynamicUl").append("<li class="+ $(this).text().toLowerCase() +">" + $(this).text() + "</li>")
+  })
+  $("#blog p.excerpt ").hide();
+}
+
+CreateTab.prototype.eventsBinding = function(){
+var that = this;
+  $(".dynamicUl>li").bind({click: that.tabFunction})
+  $(".dynamicUl>li:first").trigger("click");
+}
 
 
-$(".dynamicUl>li").bind({click: tabFunction})
-
-$(".dynamicUl>li:first").trigger("click");
-
-function tabFunction(){
- var tabtext =  $(this).text().toLowerCase();
- $(this).addClass("current")
- $("."+prevTabText).removeClass("current");
- if(prevTabText !== null || prevTabText !==undefined || prevTabText !=="")
- {
-  $("#" + prevTabText).addClass("hide");
- }
+CreateTab.prototype.tabFunction = function(){
+  var tabtext =  $(this).text().toLowerCase();
+  $(this).addClass("current")
+  $("."+ prevTabText).removeClass("current");
+  if(prevTabText !== null || prevTabText !==undefined || prevTabText !=="")
+  {
+    $("#" + prevTabText).addClass("hide");
+  }
   $("#" + tabtext).removeClass("hide");
   prevTabText = tabtext;
 }
 
-$("ul>li>h3>a").click(function(event){
-  event.preventDefault();
-   loadAjaxBlog(this);
-  $(this).parent().parent().find("p.excerpt").slideDown("slow", function(){
-    $("._current").slideUp("fast");
-    $(this).addClass("_current").slideDown()
-  })
-});
+CreateTab.prototype.showBlog = function(){
+  $("ul>li>h3>a").click(function(event){
+    event.preventDefault();
+     new LoadAjaxBlog(this);
+    $(this).parent().parent().find("p.excerpt").slideDown("slow", function(){
+      $("._current").slideUp("fast");
+      $(this).addClass("_current").slideDown()
+    })
+  });
+}
+
+CreateTab.prototype.run = function(){
+  this.tabButtons();
+  this.eventsBinding();
+  this.showBlog();
+}
+
+$(document).ready(function(){
+  new CreateTab();
+})
 
 
